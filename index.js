299310df9e31d9c91533a11c455d805bd4fa053c
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2022-03-06 23:57:05 +0530

    corns
    edited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-app.use(function(req, res, next) {
+app.use(cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -10,7 +10,6 @@ app.use(function(req, res, next) {
     next();
 });
 app.use(express.json())
-app.use(cors());
 
 
 const SignupModel =require('./models/Signup');
