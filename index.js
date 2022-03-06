969f0d38b172a7b80a5ae77ebec1f2383b14acd9
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2022-03-07 00:08:25 +0530

    tesing

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-app.use(cors(req, res) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+app.use(cors());
 app.use(express.json())
 
 
